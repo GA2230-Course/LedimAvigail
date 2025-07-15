b8376fb607d60ac422c5479db56c7d9b7c386157
Avigail Nissan <nissan.avigail01@gmail.com>
2025-07-15 14:08:39 +0300

    fixed according to Drukker's notes ex3

--- BlinkAnimation.java
+++ BlinkAnimation.java
@@ -3,18 +3,13 @@ import java.awt.Color;
 
 public class BlinkAnimation extends AnimationBase {
  private StopWatch stopWatch;
- private Color currentColor;
  private Color color1;
  private Color color2;
- 
- private double interval;
- private double lastChangeTime;
+
    public BlinkAnimation(Color color1, Color color2)
    {
-      this.currentColor = color1;
       this.color1 = color1;
       this.color2 = color2;
-      interval = 2;
    }
 
  @Override
@@ -23,21 +18,18 @@ public class BlinkAnimation extends AnimationBase {
     stopWatch = new StopWatch();
     stopWatch.start();
     strip.setAll(color1);
-    lastChangeTime = stopWatch.get();
  }
     
  @Override
  public void periodic()
  {
-    if (stopWatch.get() > lastChangeTime + interval)
+    if ((int)(stopWatch.get()) % 2 == 0)
     {
-        if (currentColor.equals(color1)) {
-            currentColor = color2;
-        } else {
-            currentColor = color1;
-        }
-        strip.setAll(currentColor);
-        lastChangeTime = stopWatch.get();
+        strip.setAll(color2);
     }
- }
+    else
+    {
+        strip.setAll(color1);
+    }
+}
 }
